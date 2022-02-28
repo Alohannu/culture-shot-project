ad4160c5a559cd6311fa8165d942880874158794
Barbara Nassar Lobato de Almeida <ba.almeida@hotmail.com>
2022-02-28 18:07:19 +0100

    created routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,74 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_161412) do
+ActiveRecord::Schema.define(version: 2022_02_28_165615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "museum_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["museum_id"], name: "index_bookmarks_on_museum_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.bigint "museum_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["museum_id"], name: "index_chatrooms_on_museum_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "museums", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "recommended_age"
+    t.time "open_start"
+    t.time "open_end"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "museums_topics", force: :cascade do |t|
+    t.bigint "museum_id", null: false
+    t.bigint "topic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["museum_id"], name: "index_museums_topics_on_museum_id"
+    t.index ["topic_id"], name: "index_museums_topics_on_topic_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.text "comment"
+    t.float "stars"
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.bigint "museum_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["museum_id"], name: "index_ratings_on_museum_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +91,24 @@ ActiveRecord::Schema.define(version: 2022_02_28_161412) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users_topics", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "topic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["topic_id"], name: "index_users_topics_on_topic_id"
+    t.index ["user_id"], name: "index_users_topics_on_user_id"
+  end
+
+  add_foreign_key "bookmarks", "museums"
+  add_foreign_key "bookmarks", "users"
+  add_foreign_key "chatrooms", "museums"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "museums_topics", "museums"
+  add_foreign_key "museums_topics", "topics"
+  add_foreign_key "ratings", "museums"
+  add_foreign_key "ratings", "users"
+  add_foreign_key "users_topics", "topics"
+  add_foreign_key "users_topics", "users"
 end

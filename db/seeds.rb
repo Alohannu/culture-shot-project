a7e0f07e4fa2a0dd0cb54a2509891485ef35638a
Malvina Smaqi <smaqi.malvina@gmail.com>
2022-03-01 15:37:28 +0100

    revision on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the database...."
+User.destroy_all
+Museum.destroy_all
 
 puts "Creating user..."
-User.create(first_name: "Sara", last_name: "Sara", email: "Sara@sara.com", password: "123456", date_of_birth: "1993,7,9")
-Museum.create(
+User.create!(first_name: "Sara", last_name: "Sara", email: "Sara@sara.com", password: "123456", date_of_birth: "1993,7,9")
+Museum.create!(
   name: "MoMa",
   address: "New York, US",
   recommended_age: "10",
   description: "amazing art pieces in downtown New York"
 )
 
-Museum.create(
+Museum.create!(
   name: "The Non Existing Club",
   address: "Nowhere",
   recommended_age: "80",

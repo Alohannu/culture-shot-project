cf911527cda4f6452fc2b39bb2dd5ad55d587596
Quoc Hanhoan Truong <tqh@posteo.net>
2022-03-01 12:15:18 +0100

    user seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(first_name: "Sara", last_name: "Sara", email: "Sara@sara.com", password: "123456", date_of_birth: "1993,7,9")

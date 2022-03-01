46da076df5a24ad120ae2afa1b21e767a7f5a63d
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-03-01 14:12:07 +0100

    bookmark-controlller-view

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to museum_path(@museum)
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.user = current_user
+
+    @museum = museum.find(params[:museum_id])
+    @bookmark.museum.id = @musuem.id
+
+    if @bookmark.save
+      redirect_to museum_path(@bookmark)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:museum_id)
+  end
+end

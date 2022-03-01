53699239fea588a58d87b2adef4a527005dae32d
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-03-01 17:16:00 +0100

    done with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,16 @@ Museum.destroy_all
 Rating.destroy_all
 
 puts "Creating user..."
-User.create!(first_name: "Sara", last_name: "Sara", email: "Sara@sara.com", password: "123456", date_of_birth: "1993,7,9")
-Museum.create!(
+user = User.new(first_name: "Sara", last_name: "Sara", email: "Sara@sara.com", password: "123456", date_of_birth: "1993,7,9")
+user.save
+
+museum = Museum.new(
   name: "Neues Museum",
   address: "Bodestraße 1-3, 10178 Berlin, Germany",
   recommended_age: "20",
   description: "your journey to ancient civilizations"
 )
+museum.save
 
 Museum.create!(
   name: "Deutsches Technikmuseum",
@@ -27,47 +30,47 @@ Museum.create!(
 )
 
 Rating.create!(
-  user_id: 1,
-  museum_id: 1,
+  user: user,
+  museum: museum,
   title: "great experience",
   comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
   stars: 3
 )
 Rating.create!(
-  user_id: 1,
-  museum_id: 1,
+  user: user,
+  museum: museum,
   title: "disappointed",
   comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
   stars: 4
 )
 
 Rating.create!(
-  user_id: 1,
-  museum_id: 1,
+  user: user,
+  museum: museum,
   title: "strongly recommend",
   comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
   stars: 5
 )
 
 Rating.create!(
-  user_id: 1,
-  museum_id: 1,
+  user: user,
+  museum: museum,
   title: "satisfactory, that's all",
   comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
   stars: 2
 )
 
 Rating.create!(
-  user_id: 1,
-  museum_id: 1,
+  user: user,
+  museum: museum,
   title: "an unforgettable journey",
   comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
   stars: 1
 )
 
 Rating.create!(
-  user_id: 1,
-  museum_id: 1,
+  user: user,
+  museum: museum,
   title: "good if you want to know more history about technology",
   comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
   stars: 2

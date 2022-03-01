6edfceb0c43b01d9d70cc7bf96e5ef3adf642a9f
Malvina Smaqi <smaqi.malvina@gmail.com>
2022-03-01 14:49:23 +0100

    created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
   end
 
   def dashboard
-    @user = current_user
-    @bookmarks = @user.bookmarks
+    @bookmarks = current_user.bookmarks
   end
 end

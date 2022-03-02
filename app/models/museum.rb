c68ce3ec4ac74dbdd83b37b1e405a7f747dcc54b
Malvina Smaqi <smaqi.malvina@gmail.com>
2022-03-02 17:57:35 +0100

    first steps of chatroom

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -2,6 +2,7 @@ class Museum < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :ratings, dependent: :destroy
   has_many :topics
+  has_one :chatroom
 
   def average
     return 0 if ratings.pluck(:stars).sum.zero?
